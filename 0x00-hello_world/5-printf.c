8659467fd495dfff75e1478f3273f7fb629e2197
Samuelolamide11 <adesanyasamuelola@gmail.com>
2023-10-07 18:05:34 -0700

    0-positive_or_negative.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
-/**
- * main -A program that prints a line with the puts funvtion
- * Return:0(success)
- */
-int main(void)
-{
-	printf("with proper grammar, but the outcome is a piece of art,/n");
-	return (0);
+int main() {
+   // printf() displays the string inside quotation
+   printf("with proper grammar, but the outcome is a piece of art,");
+   return 0;
 }
