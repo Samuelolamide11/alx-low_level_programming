bd75089150aeafbd64d19f1bed97f21c21a09d4c
Adesanyasamuelola <adesanyasamuelola@gmail.com>
2023-10-11 17:32:48 +0000

    using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- *main -program that prints 
- * programming is like building a multilingual puzzle, 
- *Return (0) success
+ *main -program that prints
+ * programming is like building a multilingual puzzle,
+ *Return: 0 (success)
  */
 int main(void)
 {
-	puts("programming is like building a multilingual puzzle,");
-	return 0;
+puts("\"programming is like building a multilingual puzzle,/n");
+	return (0);
 }
