18bd12d72f188f7cdea2fd0f4cfec83932953267
samuelolamide11 <adesanyasamuelola@gmail.com>
2024-01-12 08:44:30 -0800

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,22 +1,20 @@
-#include <stdio.h>
+#include "main.h"
+
 /**
- * _puts_recursion - main function
-
- * *s - function parameter
- * void _puts_recursion - function name
- * Return:0
- */
-
+  * _puts_recursion - main function
+  *
+  * @s: function parameter
+  *
+  * Return: Always 0.
+  */
 void _puts_recursion(char *s)
 {
-if (*s)
+	if (*s)
 	{
 	_putchar(*s);
 	_puts_recursion(s + 1);
 	}
 	else
 	_putchar('\n');
-
-
 }
 
